53128ac1abcf9393346a65c828b83d2d076211ca
Nathan Nicolessi <nathan.nicolessi@epita.fr>
2025-09-14 23:11:42 +0200

    Initial commit - base projet

---
+++ donnez-votre-avis/src/main/java/com/nathan/avis/DonnezVotreAvisApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.avis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonnezVotreAvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonnezVotreAvisApplication.class, args);
+	}
+
+}
---
+++ donnez-votre-avis/src/test/java/com/nathan/avis/DonnezVotreAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.avis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonnezVotreAvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
